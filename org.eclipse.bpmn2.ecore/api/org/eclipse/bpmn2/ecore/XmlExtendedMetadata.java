62e2dc275a73c7cf94706f2a1417a7b9bd6bce91
Reiner Hille-Doering <reiner.hille-doering@sap.com>
2010-07-07 10:27:31 +0200

    Commit initial and CQ approved contribution from https://bugs.eclipse.org/bugs/show_bug.cgi?id=316591 . This commit contains the files from "SAP Metamodel Contribution Version 2", only the folder structure is slidely changed to fit to GIT structure. Also the "edit" project is not commited here, as it does contain really relevant content. Note that this project here (org.eclipse.bpmn2.ecore) is here for reference only. For productive usage use the "refined" projects.

---
+++ org.eclipse.bpmn2.ecore/api/org/eclipse/bpmn2/ecore/XmlExtendedMetadata.java
@@ -0,0 +1,50 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) 2010 SAP AG
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Reiner Hille-Doering (SAP AG) - initial API and implementation and/or initial documentation
+ * 
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.eclipse.bpmn2.ecore;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.util.BasicExtendedMetaData;
+
+public class XmlExtendedMetadata extends BasicExtendedMetaData {
+
+	/**
+	 * 
+	 */
+	public XmlExtendedMetadata() {}
+
+	@Override
+	public String getNamespace(EPackage ePackage) {
+		/*
+		 * Unfortunately XMLSaveImpl.addNamespaceDeclarations does not consider the XML namespaces that are declared on features to
+		 * calculate correct prefixes. Instead it calls this method for all packages and assumes that namespace and prefix of the package is
+		 * correct. In our case the namespace of the package is different to the namespace of all features. We correct this my returning the
+		 * namespace of a "typical" feature.
+		 */
+		EClass docRoot = getDocumentRoot(ePackage);
+		if (docRoot != null) {
+			for (EStructuralFeature feature : docRoot.getEStructuralFeatures()) {
+				String namespace = getNamespace(feature);
+				if (namespace != null && !namespace.equals(ePackage.getNsURI()))
+					return namespace;
+			}
+		}
+
+		return super.getNamespace(ePackage);
+	}
+}
