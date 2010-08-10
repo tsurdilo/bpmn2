226c36b3e07644916bd0701bb9295bb62a521c71
Henning Heitkoetter <hheitkoetter@googlemail.com>
2010-08-10 10:19:50 +0200

    Add packages under XML namespaces to registry (plugin.xml)
    Packages are registered under their XML and XMI namespace in 
    plugin.xml, so that XML models can be loaded within Eclipse even if
    the package has not been accessed before.
    
    Add some tests to check this behaviour.
    
    Follow-up to bug 320079.

---
+++ org.eclipse.bpmn2.tests/src/org/eclipse/bpmn2/tests/LoadXMLPluginTest.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2010 Henning Heitkoetter.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Henning Heitkoetter - initial API and implementation
+ */
+package org.eclipse.bpmn2.tests;
+
+import static org.junit.Assert.fail;
+
+import org.eclipse.emf.common.EMFPlugin;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.common.util.WrappedException;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.PackageNotFoundException;
+import org.junit.Test;
+
+/**
+ * Tests if models serialized as XML or XMI can be loaded within Eclipse
+ * even if the package has not been accessed before. This ensures that all
+ * packages are correctly registered through plugin.xml.
+ * 
+ * These tests are only meaningful as plugin tests. Standalone the packages
+ * have to be registered manually anyway and are accessed during registration.
+ * 
+ * The test has to be run in isolation, as the initialization of a package can
+ * not be undone.
+ * 
+ * @author Henning Heitkoetter
+ *
+ */
+public class LoadXMLPluginTest {
+
+    @Test
+    public void testLoad() throws Exception {
+        if (!EMFPlugin.IS_ECLIPSE_RUNNING)
+            return;
+
+        try {
+            Resource r = new ResourceSetImpl().getResource(URI.createFileURI(getFilename()), true);
+        } catch (WrappedException e) {
+            if (e.exception() instanceof PackageNotFoundException)
+                fail("Package not found during load.");
+            throw e;
+        }
+    }
+
+    /**
+     * @return
+     */
+    protected String getFilename() {
+        return "res/basic.bpmn2";
+    }
+}
